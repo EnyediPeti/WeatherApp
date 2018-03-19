63cbd98dd91f0bdb2f4bfa98ffb93dff21de9234
Enyedi Péter <enyedi.peter.a@gmail.com>
2018-03-19 09:41:03 +0100

    Add details screen.

---
+++ app/src/main/java/hu/peter/enyedi/weatherapp/ui/list/details/WeatherDetailsActivity.java
@@ -0,0 +1,15 @@
+package hu.peter.enyedi.weatherapp.ui.list.details;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import hu.peter.enyedi.weatherapp.R;
+
+public class WeatherDetailsActivity extends AppCompatActivity implements WeatherDetailsScreen {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_details);
+    }
+}
---
+++ app/src/main/java/hu/peter/enyedi/weatherapp/ui/list/details/WeatherDetailsPresenter.java
@@ -0,0 +1,6 @@
+package hu.peter.enyedi.weatherapp.ui.list.details;
+
+import hu.peter.enyedi.weatherapp.ui.Presenter;
+
+public class WeatherDetailsPresenter extends Presenter<WeatherDetailsScreen> {
+}
