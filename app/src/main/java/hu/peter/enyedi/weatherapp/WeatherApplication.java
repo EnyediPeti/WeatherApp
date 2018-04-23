b6c02b9decd6f6497bed15297e25525ff4f0cbe5
Enyedi Péter <enyedi.peter.a@gmail.com>
2018-04-23 09:00:34 +0200

    Fix Application class.

--- app/src/main/java/hu/peter/enyedi/weatherapp/WeatherApplication.java
+++ app/src/main/java/hu/peter/enyedi/weatherapp/WeatherApplication.java
@@ -1,10 +1,10 @@
 package hu.peter.enyedi.weatherapp;
 
-import android.app.Application;
+import com.orm.SugarApp;
 
 import hu.peter.enyedi.weatherapp.ui.UIModule;
 
-public class WeatherApplication extends Application {
+public class WeatherApplication extends SugarApp {
 
     public static WeatherApplicationComponent injector;
 
